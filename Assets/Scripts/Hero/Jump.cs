06d45c460520664c6eb9b15a1d0c480567733c46
Corb3nik <ian@subnet192.com>
2016-08-25 10:53:28 -0400

    Adds simple jump script

---
+++ Assets/Scripts/Hero/Jump.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+	public bool isGrounded = true;
+	public float jumpPower = 1;
+	public GameObject sprite = null;
+
+	private Rigidbody2D rigidbodyComponent = null;
+
+	// Use this for initialization
+	void Start () {
+		rigidbodyComponent = sprite.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			rigidbodyComponent.AddForce(Vector2.up * jumpPower);
+			isGrounded = false;
+		}
+	}
+}
