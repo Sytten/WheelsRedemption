1b7cbc10c0015b8c1b1777e03de0d63cc2a8badb
Emile Fugulin <emilefugulin@hotmail.com>
2016-08-26 10:39:47 -0400

    Adds surface effector for platform
    Also adds triggers to change direction and changes physic behavior of hero ans walls

---
+++ Assets/Scripts/Environment/ModifySurfaceEffector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModifySurfaceEffector : MonoBehaviour {
+
+    public enum Direction { RIGHT = 1, LEFT = -1};
+
+    public int newSpeed;
+    public Direction newDirection;
+    public SurfaceEffector2D surfaceEffector;
+
+    private void OnTriggerEnter2D (Collider2D collider) {
+        if(surfaceEffector != null) {
+            surfaceEffector.speed = (int)newDirection * newSpeed;
+        }
+    }
+}
