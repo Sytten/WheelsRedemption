6de993aa26a4024855038de00a6388632326264a
Emile Fugulin <emilefugulin@hotmail.com>
2016-08-30 10:37:57 -0400

    Creates initial state interface

---
+++ Assets/Scripts/Hero/State.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public interface State {
+
+    void Start();
+
+    void Update();
+
+    void FixedUpdate();
+
+    void LateUpdate();
+
+    void OnCollisionEnter2D(Collision2D collsion);
+
+    void OnCollisionStay2D(Collision2D collsion);
+
+    void OnCollisionExit2D(Collision2D collsion);
+}
