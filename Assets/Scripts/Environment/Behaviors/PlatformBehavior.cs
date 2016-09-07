e745fe4db52df13da2f4dcc28ba826f4ffec982f
Emile Fugulin <emilefugulin@hotmail.com>
2016-09-06 22:20:21 -0400

    Moves the landing of the hero on the platform to a behavior

---
+++ Assets/Scripts/Environment/Behaviors/PlatformBehavior.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformBehavior : Behavior {
+
+    public override void Execute(InAirState state) {
+        state.LandHeroOnPlatform();
+    }
+
+}
