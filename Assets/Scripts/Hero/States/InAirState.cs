c11b66886755e11dc21f0a2a43d484c0f5195657
Emile Fugulin <emilefugulin@hotmail.com>
2016-09-06 22:57:30 -0400

    Creates the necessary functions to switch states and attach player

--- Assets/Scripts/Hero/States/InAirState.cs
+++ Assets/Scripts/Hero/States/InAirState.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class InAirState : IState {
 
-    private Hero hero;
+    private Hero hero = null;
+    private Collision2D lastCollision = null;
 
     public InAirState(Hero hero) {
         this.hero = hero;
@@ -22,18 +23,29 @@ public class InAirState : IState {
     }
 
     public virtual void OnCollisionEnter2D(Collision2D collision) {
-		if (collision.collider.tag == "StartingZone") {
-			hero.ChangeState(hero.onPlatformState);
+        lastCollision = collision;
+
+        Behavior behavior = collision.gameObject.GetComponent<Behavior>();
+
+        if (behavior != null) {
+            behavior.Execute(this);
         }
     }
 
     public virtual void OnCollisionStay2D(Collision2D collision) {
-        
     }
 
     public virtual void OnCollisionExit2D(Collision2D collision) {
     }
 
-    public void Jump(float jumpPower) {
+    public void LandHeroOnPlatform() {
+        hero.ChangeState(hero.onPlatformState);
+    }
+    
+    public void AttachHeroToLastCollision() {
+        if(lastCollision != null) {
+            hero.transform.parent = lastCollision.transform;
+            hero.ChangeState(hero.onWheelState);
+        }
     }
 }
