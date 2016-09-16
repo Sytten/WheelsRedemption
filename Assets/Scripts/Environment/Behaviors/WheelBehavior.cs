2610b551a0871f5557cc04a6ce716c6e92792240
Emile Fugulin <emilefugulin@hotmail.com>
2016-09-15 22:54:34 -0400

    Creates necessary script to change the sprite and make the arc sticky or not
    Modifies the wheel behavior to verify if the arc is sticky

--- Assets/Scripts/Environment/Behaviors/WheelBehavior.cs
+++ Assets/Scripts/Environment/Behaviors/WheelBehavior.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class WheelBehavior : Behavior {
 
+    public bool isSticky = true;
+
     public override void Execute(InAirState state) {
-        state.AttachHeroToLastCollision();
+        if(isSticky) {
+            state.AttachHeroToLastCollision();
+        }
     }
 
 }
