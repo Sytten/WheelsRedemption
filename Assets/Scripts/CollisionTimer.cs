f99cd66545e469ec56d17e561153d8f9a463e0d3
Emile Fugulin <emilefugulin@hotmail.com>
2016-09-18 23:35:43 -0400

    Fixes a bug where the player would still collide when leaving the wheel
    If the hero is in the middle of two colliders, ignoring the first collision is not enough. The use of a timer is more reliable and is not very costly (just a bool)

---
+++ Assets/Scripts/CollisionTimer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class CollisionTimer {
+
+    private bool ignore = true;
+    private int timeBeforeCollisionsEnabled;
+    private Thread timerThread;
+
+    public CollisionTimer(int timeBeforeCollisionsEnabled) {
+        this.timeBeforeCollisionsEnabled = timeBeforeCollisionsEnabled;
+        timerThread = new Thread(new ThreadStart(waitBeforeEnablingCollisions));
+    }
+
+	public void StartTimer() {
+        timerThread.Start();
+	}
+	
+	private void waitBeforeEnablingCollisions() {
+        Thread.Sleep(timeBeforeCollisionsEnabled);
+        ignore = false;
+    }
+
+    public bool IgnoreCollision() {
+        return ignore;
+    }
+
+    public void Reset() {
+        timerThread = new Thread(new ThreadStart(waitBeforeEnablingCollisions));
+        ignore = true;
+    }
+}
