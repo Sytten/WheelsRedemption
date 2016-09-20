af943d48e653105eb101e313104f4e5bc30f08f3
Emile Fugulin <emilefugulin@hotmail.com>
2016-09-19 22:12:54 -0400

    Creates basic level progression saving mechanisms

---
+++ Assets/Scripts/Data/Player/LevelStatistics.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class LevelStatistics {
+
+    public const int MAX_STARS = 3;
+
+    private int id;
+    private float bestTime;
+    private int stars;
+
+    public int GetId() {
+        return id;
+    }
+
+    public int GetStars() {
+        return stars;
+    } 
+
+    public float GetBestTime() {
+        return bestTime;
+    }
+
+    public void SetId(int Id) {
+        this.id = Id;
+    }
+
+    public void SetStars(int stars) {
+        if(stars > 0 && stars < MAX_STARS) {
+            this.stars = stars;
+        }
+    }
+
+    public void SetBestTime(float bestTime) {
+        if(bestTime > 0) {
+            this.bestTime = bestTime;
+        }
+    }
+
+    public LevelStatistics withId(int id) {
+        SetId(id);
+        return this;
+    }
+
+    public LevelStatistics withStars(int stars) {
+        SetStars(stars);
+        return this;
+    } 
+
+    public LevelStatistics withBestTime(float bestTime) {
+        SetBestTime(bestTime);
+        return this;
+    }
+}
---
+++ Assets/Scripts/Data/Player/PlayerStatistics.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class PlayerStatistics : MonoBehaviour {
+
+    private List<LevelStatistics> levelsStatistics;
+
+    public List<LevelStatistics> GetLevelsStatistics() {
+        return levelsStatistics;
+    }
+
+    public void SetLevelsStatistics(List<LevelStatistics> levelsStatistics) {
+        this.levelsStatistics = levelsStatistics;
+    }
+
+    public void SddLevelStatistics(LevelStatistics levelStatistics) {
+        if(levelsStatistics != null) {
+            LevelStatistics currentStatistics = levelsStatistics.Find(level => level.GetId() == levelStatistics.GetId());
+
+            if(currentStatistics != null) {
+                levelsStatistics.Remove(currentStatistics);
+            }
+
+            levelsStatistics.Add(levelStatistics);
+        }
+    }
+
+    public LevelStatistics SetLevelStatistics(int Id) {
+        return levelsStatistics.Find(level => level.GetId() == Id);
+    }
+}
