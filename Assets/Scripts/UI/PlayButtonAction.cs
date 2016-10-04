92a2729349b18116e008425210bc489668ab9737
Emile Fugulin <emilefugulin@hotmail.com>
2016-10-03 21:41:57 -0400

    Reworks main menu and adds transition to first level

---
+++ Assets/Scripts/UI/PlayButtonAction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayButtonAction : MonoBehaviour {
+
+	void Start () {
+        Button playButton = GetComponent<Button>();
+        playButton.onClick.AddListener(() => LevelManager.LoadLevel(1));
+	}
+}
