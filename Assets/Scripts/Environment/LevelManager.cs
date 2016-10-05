b29b26912cf8dfbbb4dfe894ab650c6b1fa3b1fb
Emile Fugulin <emilefugulin@hotmail.com>
2016-10-05 00:08:14 -0400

    Creates script to fill Levels Menu
    Adds temporary scenes
    
    Modifies levelmanager
    
    Generalizes play button action
    
    Creates UI level prefab

--- Assets/Scripts/Environment/LevelManager.cs
+++ Assets/Scripts/Environment/LevelManager.cs
@@ -1,6 +1,5 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class LevelManager
 {
@@ -15,5 +14,23 @@ public class LevelManager
 
     public static void LoadMainMenu() {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public static void LoadLevelsMenu() {
+        SceneManager.LoadScene("LevelsMenu");
+    }
+
+    public static List<int> getLevelsID() {
+        List<int> levelsID = new List<int>();
+
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; ++i) {
+            int id = 0;
+            string levelName = SceneManager.GetSceneAt(i).name;
+            if (int.TryParse(levelName, out id)) {
+                levelsID.Add(id);
+            }
+        }
+
+        return levelsID;
     }
 }
---
+++ Assets/Scripts/UI/LevelsDisplay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LevelsDisplay : MonoBehaviour {
+
+    public GameObject levelButtonPrefab;
+
+	void Start () {
+        List<int> levelsID = LevelManager.getLevelsID();
+
+        foreach (int levelID in levelsID) {
+            GameObject levelButton = Instantiate(levelButtonPrefab) as GameObject;
+
+            levelButton.GetComponent<LevelButtonAction>().levelID = levelID;
+
+            levelButton.transform.parent = transform;
+        }
+	}
+}
