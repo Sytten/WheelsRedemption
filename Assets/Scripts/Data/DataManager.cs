4c127fad70e2c4da835c5d59aff0803db4facddd
Emile Fugulin <emilefugulin@hotmail.com>
2016-10-09 13:42:10 -0400

    Fixes build failure because of editor script in general build

--- Assets/Scripts/Data/DataManager.cs
+++ Assets/Scripts/Data/DataManager.cs
@@ -1,16 +1,11 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
+﻿using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.IO;
-using UnityEditor;
-using System.Linq;
-using System.Text.RegularExpressions;
+using UnityEngine;
 
 public class DataManager {
 
     private static string PLAYER_STATISTICS_FILE = Application.dataPath + "/playerStatistics.bin";
     private static string BUILD_SCENES_NAMES_FILE = Application.dataPath + "/ScenesNamesList.asset";
-    private static string SCENE_NAME_REGEX = @"([^/]*/)*([\w\d\-]*)\.unity";
 
     private static BinaryFormatter binaryFormatter = new BinaryFormatter();
 
@@ -41,16 +36,5 @@ public class DataManager {
         }
 
         return scenesNamesList;
-    }
-
-    [MenuItem("Assets/Save Build Scenes Names %e")]
-    private static void SaveBuildScenesNames() {
-        List<string> buildScenesNames = EditorBuildSettings.scenes.Select(scene => scene.path).ToList();
-
-        Regex sceneNameRegex = new Regex(SCENE_NAME_REGEX);
-        buildScenesNames = buildScenesNames.Select(sceneName => sceneNameRegex.Replace(sceneName, "$2")).ToList();
-
-        FileStream file = File.Create(BUILD_SCENES_NAMES_FILE);
-        binaryFormatter.Serialize(file, new ScenesNamesList().withScenesNames(buildScenesNames));
     }
 }
---
+++ Assets/Scripts/Data/Editor/DataManagerEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+public class DataManagerEditor : MonoBehaviour {
+
+    private static string BUILD_SCENES_NAMES_FILE = Application.dataPath + "/ScenesNamesList.asset";
+    private static string SCENE_NAME_REGEX = @"([^/]*/)*([\w\d\-]*)\.unity";
+
+    private static BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+    [MenuItem("Assets/Save Build Scenes Names %e")]
+    private static void SaveBuildScenesNames() {
+        List<string> buildScenesNames = EditorBuildSettings.scenes.Select(scene => scene.path).ToList();
+
+        Regex sceneNameRegex = new Regex(SCENE_NAME_REGEX);
+        buildScenesNames = buildScenesNames.Select(sceneName => sceneNameRegex.Replace(sceneName, "$2")).ToList();
+
+        FileStream file = File.Create(BUILD_SCENES_NAMES_FILE);
+        binaryFormatter.Serialize(file, new ScenesNamesList().withScenesNames(buildScenesNames));
+    }
+}
