294e0bd36fab341b1328c89042186bb70145b2d9
Emile Fugulin <emilefugulin@hotmail.com>
2016-10-17 20:33:18 -0400

    Fixes some problems with static class

--- Assets/Scripts/UI/InGameMenuDisplay.cs
+++ Assets/Scripts/UI/InGameMenuDisplay.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 
-public class InGameMenuDisplay : MonoBehaviour {
+public static class InGameMenuDisplay {
 
     public static void LoadWinMenu() {
         GameObject menuFramework = loadMenuFramework();
 
-        GameObject winMenu = Instantiate(Resources.Load("Win Menu") as GameObject);
+        GameObject winMenu = GameObject.Instantiate(Resources.Load("Win Menu") as GameObject);
 
         winMenu.transform.SetParent(menuFramework.GetComponentInChildren<RectTransform>(), false);
     }
@@ -13,14 +13,14 @@ public class InGameMenuDisplay : MonoBehaviour {
     public static void LoadLoseMenu() {
         GameObject menuFramework = loadMenuFramework();
 
-        GameObject loseMenu = Instantiate(Resources.Load("Lose Menu") as GameObject);
+        GameObject loseMenu = GameObject.Instantiate(Resources.Load("Lose Menu") as GameObject);
 
         loseMenu.transform.SetParent(menuFramework.GetComponentInChildren<RectTransform>(), false);
 
     }
 
     private static GameObject loadMenuFramework() {
-        GameObject menuFramework = Instantiate(Resources.Load("Menu Framework") as GameObject);
+        GameObject menuFramework = GameObject.Instantiate(Resources.Load("Menu Framework") as GameObject);
 
         menuFramework.GetComponentInChildren<Canvas>().worldCamera = Camera.current;
 
