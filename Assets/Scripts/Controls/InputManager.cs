eb28035675ab0259e47146e7182c001bf21e5379
Emile Fugulin <emilefugulin@hotmail.com>
2016-10-19 23:32:06 -0400

    Fixes input checking

--- Assets/Scripts/Controls/InputManager.cs
+++ Assets/Scripts/Controls/InputManager.cs
@@ -9,17 +9,14 @@ public class InputManager : MonoBehaviour {
 
     private void Update() {
 #if UNITY_EDITOR
-        if (Input.GetKeyUp(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space)) {
             EventManager.Publish(new InputEvent().withTouch(DEFAULT_TOUCH));
         }
 #elif UNITY_ANDROID
-        if (Input.touchCount == 0 && previousTouchCount > 0) {
-            previousTouchCount = Input.touchCount;
+        if (Input.touchCount > 0 && previousTouchCount == 0) {
             EventManager.Publish(new InputEvent().withTouch(DEFAULT_TOUCH));
-        } else if (Input.touchCount > 0 && previousTouchCount == 0) {
-            previousTouchCount = Input.touchCount;
-            EventManager.Publish(new InputEvent().withTouch(DEFAULT_TOUCH)); return true;
-        }
+        } 
+        previousTouchCount = Input.touchCount;
 #endif
     }
 }
