9096945e46a6b646d98d7e8506563cc51222f1d7
Emile Fugulin <emilefugulin@hotmail.com>
2016-10-30 12:47:28 -0400

    Adds detroyable wheels
    Also adds tests scenes

---
+++ Assets/Scripts/Environment/Destroy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+    public Behavior behaviorToRemove;
+
+    private void Start() {
+        if (behaviorToRemove == null) {
+            behaviorToRemove = GetComponent<Behavior>();
+        }
+    }
+
+    private void OnCollisionExit2D() {
+        Destroy(behaviorToRemove);
+        EventManager.Publish(new DestroyEvent().withInstanceId(gameObject.GetInstanceID()));
+        enabled = false;
+    }
+}
